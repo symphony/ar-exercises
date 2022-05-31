da6ec9880707c814fc2c4425356454bbacdeed20
symphony <jciurc@gmail.com>
2022-05-31 12:30:58 -0600

    add employees seed and relations

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,14 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "yes", last_name: "hey", hourly_rate: 32)
+@store2.employees.create(first_name: "hello", last_name: "me", hourly_rate: 50)
+@store2.employees.create(first_name: "potato", last_name: "noodle", hourly_rate: 600)
+
+@store1.save
+@store2.save
+
+puts "total employees: #{Employee.count}"
+
+puts "- done ex 6 - store count: #{Store.count}"
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,3 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
