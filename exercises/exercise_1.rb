84388fdeee1efc8ffb98553a6c03acae4065d50d
symphony <jciurc@gmail.com>
2022-05-31 11:41:45 -0600

    clone project, install gems, seed stores table with 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,24 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+bby = Store.new
+bby.name = "Burnaby"
+bby.annual_revenue = 300000
+bby.mens_apparel = true
+bby.womens_apparel = true
+
+rch = Store.new
+rch.name = "Richmond"
+rch.annual_revenue = 1260000
+rch.mens_apparel = false
+rch.womens_apparel = true
+
+gas = Store.new
+gas.name = "Gastown"
+gas.annual_revenue = 190000
+gas.mens_apparel = true
+gas.womens_apparel = false
+
+bby.save
+rch.save
+gas.save
